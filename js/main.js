7b7be9e0966ae61f8073699e73f241c6d87a9252
Ekaterina Krivosheeva <99811665+GaikaKat@users.noreply.github.com>
2022-11-16 21:31:40 +0100

    почти готовое 4й блок

--- js/main.js
+++ js/main.js
@@ -15,6 +15,14 @@ const MIN_ID_NUMBER = 1;
 const MAX_ID_AVATAR = 10;
 const MIN_COST_PRICE = 1;
 const MAX_COST_PRICE = 10000;
+const MIN_NUMBER_ROOMS = 1;
+const MAX_NUMBER_ROOMS = 5;
+const MIN_NUMBER_GUEST = 1;
+const MAX_NUMBER_GUEST = 6;
+const LOCATION_LAT_MIN = 35.65000;
+const LOCATION_LAT_MAX = 35.70000;
+const LOCATION_LNG_MIN = 139.70000;
+const LOCATION_LNG_MAX = 139.80000;
 
 const TITLE = [
   'крутая квартира',
@@ -30,67 +38,72 @@ const TYPE = [
   'hotel'
 ];
 
-const ROOMS = [
-  1,
-  2,
-  3,
-  4
+
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const DESCRIPTION = [
+  'Большой шатер-палатка',
+  'Бомж-стайл',
+  'Займемся любовью прямо у входа',
+  'Сюда будет страшно зайти',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
 
 const idNumber = (() => {
   const result = [];
   for(let i= 1; i<=MAX_ID_AVATAR; i++){
-    result[i] = i.padStart(2, 0);
+    result[i] = String(i).padStart(2, 0);
   }
   return result;
 })();
 console.log(idNumber);
 
 
-const creatingLandlordCard = () => {
-  const author = {
-    avatar: `img/avatars/user${getRandomPositiveInteger(MIN_ID_NUMBER, idNumber)}.png`,
-  };
+const creatingLandlordCard = () => ({
+  return: {
+    autor: {
+      avatar: `img/avatars/user${getRandomPositiveInteger(MIN_ID_NUMBER, idNumber)}.png`},
+    offer: {
+      title: getRandomPositiveInteger(TITLE),
+      address: `${location.lat}, ${location.lng}`,
+      price: getRandomPositiveInteger(MIN_COST_PRICE, MAX_COST_PRICE),
+      type: getRandomPositiveInteger(TYPE),
+      rooms:getRandomPositiveInteger(MIN_NUMBER_ROOMS, MAX_NUMBER_ROOMS),
+      guests: getRandomPositiveInteger(MIN_NUMBER_GUEST, MAX_NUMBER_GUEST),
+      checkin: getRandomPositiveInteger(CHECKIN),
+      checkout: getRandomPositiveInteger(CHECKOUT),
+      features: getRandomPositiveInteger(FEATURES),
+      description: getRandomPositiveInteger(DESCRIPTION),
+      photos: getRandomPositiveInteger(PHOTOS),
+      location: {lat: getRandomPositiveFloat(LOCATION_LAT_MIN, LOCATION_LAT_MAX),
+        lng: getRandomPositiveFloat(LOCATION_LNG_MIN, LOCATION_LNG_MAX)}
 
-  const offer = () => ({
-    return: {
-      title : getRandomPositiveInteger(TITLE),
-      address : `${location.lat}` + ','+ ' '+ `${location.lng}`,
-      price : getRandomPositiveInteger(MIN_COST_PRICE, MAX_COST_PRICE),
-      type : getRandomPositiveInteger(TYPE),
-      palace: getRandomPositiveInteger(ROOMS),
-      flat,
-      house,
-      bungalow,
-      hotel
+
     }
-  });
-};
+  }});
 
-
-/* offer, объект — содержит информацию об объявлении. Состоит из полей:
-title, строка — заголовок предложения. Придумайте самостоятельно.
-address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.lat}}, {{location.lng}}.
-price, число — стоимость. Случайное целое положительное число.
-type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
-
-rooms, число — количество комнат. Случайное целое положительное число.
-
-guests, число — количество гостей, которое можно разместить. Случайное целое положительное число.
-
-checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-
-checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-
-features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
-
-description, строка — описание помещения. Придумайте самостоятельно.
-
-photos, массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
-
-location, объект — местоположение в виде географических координат. Состоит из двух полей:
-
-lat, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
-
-lng, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000.
-*/
+console.log(creatingLandlordCard());
